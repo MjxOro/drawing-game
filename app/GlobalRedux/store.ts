1c1e9f7d6a95667e4021e1d3172f980c9b88cbe3
MjxOro <mattheweoro@gmail.com>
2023-04-11 13:40:50 -0400

    refactor: remove landing page store

--- app/GlobalRedux/store.ts
+++ app/GlobalRedux/store.ts
@@ -3,14 +3,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Features/auth/authSlice';
 import modalReducer from './Features/modal/modalSlice';
-import landingReducer from './Features/landing/landingSlice';
-import type { TypedUseSelectorHook } from 'react-redux'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     modal: modalReducer,
-    landing: landingReducer
   },
 });
 
